900ea62608dc94b6dd29cb4c36d3957aee8e959b
SaradaSymonds <symonds.s@husky.neu.edu>
2018-01-26 22:02:47 -0500

    initial node project

---
+++ beantown/routes/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+
+const defaultParameters = {
+  'textToAnalyze':
+      'Call me Ishmael. Some years ago-never mind how long precisely-having \
+      little or no money in my purse, and nothing particular to interest \
+      me on shore, I thought I would sail about a little and see the \
+      watery part of the world. It is a way I have of driving off the \
+      spleen and regulating the circulation. Whenever I find myself \
+      growing grim about the mouth; whenever it is a damp, drizzly \
+      November in my soul; whenever I find myself involuntarily pausing \
+      before coffin warehouses, and bringing up the rear of every \
+      funeral I meet; and especially whenever my hypos get such an upper \
+      hand of me, that it requires a strong moral principle to prevent \
+      me from deliberately stepping into the street, and methodically \
+      knocking people\'s hats off-then, I account it high time to get to \
+      sea as soon as I can. This is my substitute for pistol and ball. \
+      With a philosophical flourish Cato throws himself upon his sword; \
+      I quietly take to the ship. There is nothing surprising in this. \
+      If they but knew it, almost all men in their degree, some time or \
+      other, cherish very nearly the same feelings towards the ocean \
+      with me. There now is your insular city of the Manhattoes, belted \
+      round by wharves as Indian isles by coral reefs-commerce surrounds \
+      it with her surf. Right and left, the streets take you waterward.',
+  'username':      '"4966fd78-48c9-4a5d-a258-d374138fc5dd',
+  'password':      'i0mSXScBcdph',
+  'url' : 'https://sandbox-watson-proxy.mybluemix.net/personality-insights/api',
+  'use_unauthenticated' : true
+}
+
+function get_insights(params) {
+  var res = {};
+
+  const PersonalityInsightsV3 =
+      require('watson-developer-cloud/personality-insights/v3');
+
+  var url = params.url || 'https://gateway.watsonplatform.net/personality-insights/api' ;
+  var use_unauthenticated =  params.use_unauthenticated || false ;
+
+  const personality_insights = new PersonalityInsightsV3({
+      'username': params.username,
+      'password': params.password,
+      'version_date': '2016-05-20',
+      'url' : url,
+      'use_unauthenticated': use_unauthenticated
+    });
+
+    personality_insights.profile({'text': params.textToAnalyze}, function(err, res) {
+      if (err)
+        reject(err);
+      else
+        return res;
+
+    });
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var insights = get_insights(defaultParameters);
+  res.render('index', { title: insights});
+});
+
+module.exports = router;
