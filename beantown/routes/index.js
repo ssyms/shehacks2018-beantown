514f3ee262202578f3633d3de74c163ef4aa1702
SaradaSymonds <symonds.s@husky.neu.edu>
2018-01-27 02:35:23 -0500

    got twitter profiles working

--- beantown/routes/index.js
+++ beantown/routes/index.js
@@ -31,6 +31,26 @@ const defaultParameters = {
 
 var my_insights = '';
 
+function parentId(tweet) {
+  if (tweet.in_reply_to_screen_name != null) {
+    return tweet.in_reply_to_user_id;
+  } else if (tweet.retweeted && (tweet.current_user_retweet != null)) {
+    return tweet.current_user_retweet.id_str;
+  }
+};
+
+function toContentItem(tweet) {
+  return {
+    id: tweet.id_str,
+    language: tweet.lang,
+    contenttype: 'text/plain',
+    content: tweet.text.replace('[^(\\x20-\\x7F)]*',''),
+    created: Date.parse(tweet.created_at),
+    reply: tweet.in_reply_to_screen_name != null,
+    parentid: parentId(tweet)
+  };
+};
+
 function get_insights(params) {
   var res = {};
 
@@ -66,7 +86,23 @@ function get_insights(params) {
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var insights = get_insights(defaultParameters);
-  console.log(my_insights);
+
+  var Twit = require('twit');
+  var T = new Twit({
+    consumer_key: 'pY2FCyJlHkbfZh5k7zzTAl957',
+    consumer_secret: 'lehQuMJRkae5kjEkn67WKIcjaHX37iwsLkOtwzDtyp2wMEH4Bx',
+    access_token: '155794248-o0GuQi1zdyTZGmR9XeGJEO0X3NWljbhGzNCQSqI6',
+    access_token_secret: 'i0JA19er0lYZWZkerCarEF7WGotkunFHdBKUkt7axvsXy'
+  })
+
+  var options = { screen_name: 'billnye',
+                  count: 600 };
+
+  T.get('statuses/user_timeline', options , function(err, data) {
+    var params = {};
+    params.content_items = data.map(toContentItem);
+    get_insights(params);
+  });
   res.render('index', { title: 'Express', json: my_insights});
 });
 
